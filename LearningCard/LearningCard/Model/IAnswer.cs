3c8fa84f81cad7385535770acef94b02df318c1b
Akomer <spdrhu@gmail.com>
2015-04-12 19:47:05 +0200

    Add basic Q&A interfaces

---
+++ LearningCard/LearningCard/Model/IAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCard.Model
+{
+    interface IAnswer
+    {
+        public String getAnswer();
+        public Boolean checkAnswer();
+    }
+}
