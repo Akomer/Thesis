850b86399837578300577cafb8b536108fbb1f4d
Akomer <spdrhu@gmail.com>
2015-05-15 21:57:30 +0200

    Save berfor major changes in CardPack saving

--- LearningCard/LearningCard/Model/CardPack.cs
+++ LearningCard/LearningCard/Model/CardPack.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace LearningCard.Model
 {
@@ -14,5 +16,90 @@ namespace LearningCard.Model
         public String PackName { get; set; }
         [DataMember]
         public List<Card> CardList { get; set; }
+
+        static List<CardPack> CardPackList()
+        {
+            List<CardPack> deckList = new List<CardPack>();
+            deckList = new List<CardPack>();
+            String path = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName;
+            path += "\\CardPacks\\";
+            foreach (String file in Directory.GetFiles(path, "*.lcp"))
+            {
+
+                //deckList.Add();
+            }
+            return deckList;
+        }
+
+        static CardPack LoadCardPackFromFile(String deckName)
+        {
+            CardPack tmpDeck;
+            String path;
+            if (deckName.Contains('\\'))
+            {
+                path = deckName;
+            }
+            else
+            {
+                path = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName;
+                path += "\\CardPacks\\" + deckName + ".lcp";
+            }
+            using (FileStream fStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(CardPack));
+                //try
+                {
+                    tmpDeck = (CardPack)serializer.ReadObject(fStream);
+                }
+            }
+            return tmpDeck;
+        }
+
+        static void SaveCardPackToFile(CardPack deck)
+        {
+            String BasePath = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName;
+            String ImagePath = BasePath + "\\CardPacks\\" + deck.PackName + "_IMG";
+            String FilePath = BasePath + "\\CardPacks\\" + deck.PackName + ".lcp"; ;
+
+            if (!System.IO.Directory.Exists(ImagePath))
+            {
+                System.IO.Directory.CreateDirectory(ImagePath);
+            }
+
+            foreach (String item in System.IO.Directory.GetFiles(ImagePath))
+            {
+                System.IO.File.Delete(item);
+            }
+
+            HashSet<String> imgSet = new HashSet<String>();
+            foreach (Card item in deck.CardList)
+            {
+                if (item.Question.GetQuestionType() == typeof(QuestionPictureModel))
+                {
+                    QuestionPictureModel q = (QuestionPictureModel)item.Question;
+                    if (q.ImageSRC.IsAbsoluteUri)
+                    {
+                        String BaseImgName = System.IO.Path.GetFileNameWithoutExtension(q.ImageSRC.ToString());
+                        String ImgExtension = System.IO.Path.GetExtension(q.ImageSRC.ToString());
+                        String imgName = BaseImgName+ImgExtension;
+                        while (imgSet.Contains(imgName))
+                        {
+                            Int32 i = 0;
+                            imgName = BaseImgName + String.Format("_{0}.{1}", i, ImgExtension);
+                            i += 1;
+                        }
+                        System.IO.File.Copy(q.ImageSRC.ToString(), ImagePath + imgName);
+                        imgSet.Add(imgName);
+                        q.ImageSRC = new Uri("\\CardPacks\\" + deck.PackName + "_IMG\\" + imgName, UriKind.Relative);
+                    }
+                }
+            }
+
+            using (FileStream saveFile = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(CardPack));
+                serializer.WriteObject(saveFile, deck);
+            }
+        }
     }
 }
